8fcef17156e490112c91ef0e718ec086c39fad0c
TheJudgeMonkey <mr.stas.anufriev@gmail.com>
2021-07-01 16:46:46 +0300

    Add README.md and add palindrom.rb

---
+++ README.md
@@ -0,0 +1,2 @@
+## Project - "Palindrome" by course The Odin Project
+**This is a simple "*Palindrome check" Ruby**
---
+++ palindrom.rb
@@ -0,0 +1,7 @@
+def palind(word)
+    if word == word.reverse
+        puts "palindrom"
+    else
+        puts "ne palindrom"
+    end
+end
